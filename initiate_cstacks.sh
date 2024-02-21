112409fa4571b224387b0fbd02a156edce8fe779
Miranda Sherlock <116809046+mirandasherlock@users.noreply.github.com>
2024-02-21 11:59:55 +0000

    Create initiate_cstacks.sh
    Script for creating a catalog using cstacks.

---
+++ initiate_cstacks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+#$ -l h_rt=48:0:0
+#$ -l mem=48G
+#$ -pe smp 32
+#$ -l tmpfs=15G
+#$ -N cstacks_job
+#$ -wd /home/username/Scratch/stacks_directory/cstacks_out
+
+module load stacks/2.54/gnu-4.9.2
+
+cd /home/username/Scratch/stacks_directory
+
+popmap=popmap.txt 
+# standards STACKS popmap, one individiaul per line with tab space to population ID. See manual for more detail. 
+
+INPUT=/home/username/Scratch/stacks_directory/ustacks_out
+
+n=3
+# Change for the value of n that you selected via optimisation steps. 
+
+out_dir=/home/username/Scratch/stacks_directory/cstacks_out 
+
+log_file=$out_dir/denovo_map.oe
+
+/usr/bin/time --verbose cstacks -P $INPUT -M $popmap -n $n -p $NSLOTS
